e6c6a9f2702d8c3759c4936b922f85b8d6436788
Tuomo Heino <tuomo.heino@hotmail.com>
2017-09-07 21:53:35 +0300

    Improve searching performance
    Improves searching performance by threadhing
    larger search requests.

--- app/src/main/kotlin/xyz.hetula.homefy/library/HomefyLibrary.kt
+++ app/src/main/kotlin/xyz.hetula.homefy/library/HomefyLibrary.kt
@@ -31,6 +31,10 @@ import android.util.Log
 import xyz.hetula.homefy.player.Song
 import xyz.hetula.homefy.service.Homefy
 import java.util.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 /**
@@ -46,6 +50,7 @@ import kotlin.collections.ArrayList
  * @since 1.0
  */
 class HomefyLibrary {
+    private val mSearchExecutor = Executors.newCachedThreadPool()
     private var mSongDatabase: MutableMap<String, Song>? = null
     private val mArtistCache = HashMap<String, ArrayList<Song>>()
     private val mAlbumCache = HashMap<String, ArrayList<Song>>()
@@ -83,6 +88,14 @@ class HomefyLibrary {
     }
 
     fun release() {
+        mSearchExecutor.shutdown()
+        try {
+            mSearchExecutor.awaitTermination(3, TimeUnit.SECONDS)
+        } catch (ex: Exception) {
+            Log.e(TAG, "Interrupt when waiting.", ex)
+        }
+        mSearchExecutor.shutdownNow()
+
         mSongDatabase?.clear()
         mArtistCache.clear()
         mAlbumCache.clear()
@@ -134,54 +147,125 @@ class HomefyLibrary {
     @Synchronized
     fun search(search: String, type: SearchType, callback: (List<Song>) -> Unit) {
         mSearchTask?.cancel(true)
-        mSearchTask = SearchTask(mMusic!!, callback)
+        mSearchTask = SearchTask(mMusic!!, mSearchExecutor, callback)
         mSearchTask?.execute(SearchRequest(search, type))
     }
 
     data class SearchRequest(val search: String, val type: SearchType)
 
-    private class SearchTask(val music: List<Song>, val callback: (List<Song>) -> Unit) :
+    private class SearchTask(val music: List<Song>, val executor: Executor,
+                             val callback: (List<Song>) -> Unit) :
             AsyncTask<SearchRequest, Void, List<Song>>() {
 
         override fun doInBackground(vararg params: SearchRequest?): List<Song> {
             val req = params[0]!!
             val start = SystemClock.elapsedRealtime()
-            if (isCancelled) return Collections.emptyList()
-            val result = search(req.search, req.type)
+            val result = search(req.search.toLowerCase(), req.type)
             Log.d(TAG, "Search done in: ${(SystemClock.elapsedRealtime() - start)} ms")
-            if (isCancelled) return Collections.emptyList()
             return result
         }
 
         override fun onPostExecute(result: List<Song>?) {
-            if (result != null && !result.isEmpty()) {
+            if (result != null) {
                 callback(result)
             }
         }
 
         private fun search(search: String, type: SearchType): List<Song> {
-            val res = ArrayList<Song>()
-            for (song in music) {
-                if (isCancelled) return Collections.emptyList()
+            val results = ArrayList<Song>()
+            val workers = getThreadCount(music.size)
+
+            if (workers == 1) {
+                processAll(results, search, type)
+            } else {
+                lickitySplit(workers, results, search, type)
+            }
+            return results
+        }
+
+        private fun processAll(results: MutableList<Song>, search: String, type: SearchType) {
+            SearchWorker(music, search, type, this::isCancelled) {
+                results.addAll(it)
+            }.run()
+        }
+
+        private fun lickitySplit(workers: Int,
+                                 results: MutableList<Song>,
+                                 search: String, type: SearchType) {
+            val reqSize = music.size / workers
+            val latch = CountDownLatch(workers)
+            var lastIndex = 0
+            for (i in 0 until workers - 1) {
+                lastIndex = i * reqSize + reqSize
+                val list = music.subList(i * reqSize, lastIndex)
+                executor.execute(SearchWorker(list, search, type, this::isCancelled, latch) {
+                    synchronized(results) {
+                        results.addAll(it)
+                    }
+                })
+            }
+            val list = music.subList(lastIndex, music.size)
+            SearchWorker(list, search, type, this::isCancelled, latch) {
+                synchronized(results) {
+                    results.addAll(it)
+                }
+            }.run()
+
+            latch.await()
+            if (isCancelled) return
+            results.sort()
+        }
+
+        private fun getThreadCount(size: Int): Int {
+            if (size < 500) {
+                return 1
+            }
+            if (size < 1000) {
+                return 2
+            }
+            return Math.max(3, Runtime.getRuntime().availableProcessors())
+        }
+    }
+
+    private class SearchWorker(val searchList: List<Song>,
+                               val search: String,
+                               val type: SearchType,
+                               val cancel: () -> Boolean,
+                               val latch: CountDownLatch? = null,
+                               val callback: (List<Song>) -> Unit) : Runnable {
+        override fun run() {
+            val results = ArrayList<Song>()
+            for (song in searchList) {
+                if (cancel()) {
+                    latch?.countDown()
+                    return
+                }
                 if (filter(song, search, type)) {
-                    res.add(song)
+                    results.add(song)
                 }
             }
-            return res
+            callback(results)
+            latch?.countDown()
         }
 
         private fun filter(song: Song, search: String, type: SearchType): Boolean {
             return when (type) {
-                SearchType.TITLE -> song.title.contains(search, true)
-                SearchType.ALBUM -> song.album.contains(search, true)
-                SearchType.ARTIST -> song.artist.contains(search, true)
-                SearchType.GENRE -> song.genre.contains(search, true)
-                else -> song.title.contains(search, true) ||
-                        song.album.contains(search, true) ||
-                        song.artist.contains(search, true)
+                SearchType.TITLE -> contains(song.title, search)
+                SearchType.ALBUM -> contains(song.album, search)
+                SearchType.ARTIST -> contains(song.artist, search)
+                SearchType.GENRE -> contains(song.genre, search)
+                else -> contains(song.title, search) ||
+                        contains(song.album, search) ||
+                        contains(song.artist, search)
             }
         }
 
+        private fun contains(value: String, search: String): Boolean {
+            if (search.length > value.length) {
+                return false
+            }
+            return value.toLowerCase().contains(search)
+        }
     }
 
     companion object {
